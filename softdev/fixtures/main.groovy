fa0bbdf792f6189618e1691f8d6a0492db701769
pom6717964 <pommink@gmail.com>
2015-10-28 20:32:53 +0700

    เพิ่มข้อมูลใน fixtures
    - เพิ่มข้อมูลรายการอุปกรณ์ต่างๆเข้ามา เพื่อให้ผู้ใช้เลือกยืมได้มากขึ้น
    - แก้ไขชื่อ user

--- softdev/fixtures/main.groovy
+++ softdev/fixtures/main.groovy
@@ -5,11 +5,11 @@ fixture {
 	role_user(Role, authority: "ROLE_USER")
 
 	user_admin(Account, username: "admin", name:"admin", barcode: "1234", password: "1234")
-	user01(Account, username: "user01", name:"user01", barcode: "1234", password: "1234")
-	user02(Account, username: "user02", name:"user02", barcode: "1234", password: "1234")
-	user03(Account, username: "user03", name:"user03", barcode: "1234", password: "1234")
-	user04(Account, username: "user04", name:"user04", barcode: "1234", password: "1234")
-	user05(Account, username: "user05", name:"user05", barcode: "1234", password: "1234")
+	user01(Account, username: "user01", name:"เทพนิว", barcode: "1234", password: "1234")
+	user02(Account, username: "user02", name:"นิว", barcode: "1234", password: "1234")
+	user03(Account, username: "user03", name:"โจ้", barcode: "1234", password: "1234")
+	user04(Account, username: "user04", name:"ซิก", barcode: "1234", password: "1234")
+	user05(Account, username: "user05", name:"จ๊อย", barcode: "1234", password: "1234")
 
 	user_role_admin(AccountRole, account: user_admin, role: role_admin)
 	user_role_user01(AccountRole, account: user01, role: role_user)
@@ -19,12 +19,23 @@ fixture {
 	user_role_user05(AccountRole, account: user05, role: role_user)
 
 
-	e01(Equipment, barcode:"0000",title:"pencil" ,amount:20	,price:10 ,description:"มันคือดินสอ")
-	e02(Equipment, barcode:"0001",title:"pen" ,amount:20	,price:10 ,description:"")
-	e03(Equipment, barcode:"0002",title:"key" ,amount:20	,price:10 ,description:"")
+	e01(Equipment, barcode:"0000",title:"ดินสอ" ,amount:20	,price:10 ,description:"ดินสอกดเปลี่ยนไส้ได้")
+	e02(Equipment, barcode:"0001",title:"ปากกาน้ำเงิน" ,amount:30	,price:10 ,description:"ปากกาเขียนสมุดน้ำหมึกน้ำเงิน")
+	e03(Equipment, barcode:"0002",title:"ปากกาแดง" ,amount:30	,price:10 ,description:"ปากกาเขียนสมุดน้ำหมึกสีแดง")
+	e04(Equipment, barcode:"0003",title:"ชอล์ก" ,amount:25	,price:5 ,description:"ชอล์กไร้ฝุ่น")
+	e05(Equipment, barcode:"0004",title:"แม็ก" ,amount:8	,price:20 ,description:"สำหรับเย็บกระดาษ")
+	e06(Equipment, barcode:"0005",title:"ลูกแม็ก" ,amount:15	,price:15 ,description:"สำหรับใส่แม็ก")
+	e07(Equipment, barcode:"0006",title:"กาว" ,amount:10	,price:25 ,description:"สำหรับติดกระดาษ")
+	e08(Equipment, barcode:"0007",title:"กรรไกร" ,amount:8	,price:20 ,description:"สำหรับตัดกระดาษ")
+	e09(Equipment, barcode:"0008",title:"กระดาษA4" ,amount:10	,price:100 ,description:"กระดาษ A4 1รีม")
+	e10(Equipment, barcode:"0009",title:"ถุงดำ" ,amount:25	,price:49 ,description:"สำหรับใส่ขยะ")
+	e11(Equipment, barcode:"0010",title:"น้ำยาถูพื้น" ,amount:10	,price:75 ,description:"สำหรับขัดห้องน้ำ")
+	
 
-	r01(Requistion, requistionDate :new LocalDate(), returnDate :new LocalDate().plusDays(7),amount:2,equipments :[e01],borrower: user01, endorser: user_admin, approved : true)
-	r02(Requistion, requistionDate :new LocalDate(), returnDate :new LocalDate().plusDays(7),amount:2,equipments :[e01, e02],borrower: user02, endorser: user_admin, approved : true)
-	r03(Requistion, requistionDate :new LocalDate(), returnDate :new LocalDate().plusDays(7),amount:2,equipments :[e01, e02, e03],borrower: user03, endorser: user_admin, approved : true)
-
+	r01(Requistion, requistionDate :new LocalDate(), returnDate :new LocalDate().plusDays(7),amount:2,equipments :[e01, e02, e03, e04, e05, e06, e07, e08, e09, e10, e11],borrower: user01, endorser: user_admin, approved : true)
+	r02(Requistion, requistionDate :new LocalDate(), returnDate :new LocalDate().plusDays(7),amount:2,equipments :[e01, e02, e03, e04, e05, e06, e07, e08, e09, e10, e11],borrower: user02, endorser: user_admin, approved : true)
+	r03(Requistion, requistionDate :new LocalDate(), returnDate :new LocalDate().plusDays(7),amount:2,equipments :[e01, e02, e03, e04, e05, e06, e07, e08, e09, e10, e11],borrower: user03, endorser: user_admin, approved : true)
+	r04(Requistion, requistionDate :new LocalDate(), returnDate :new LocalDate().plusDays(7),amount:2,equipments :[e01, e02, e03, e04, e05, e06, e07, e08, e09, e10, e11],borrower: user04, endorser: user_admin, approved : true)
+	r05(Requistion, requistionDate :new LocalDate(), returnDate :new LocalDate().plusDays(7),amount:2,equipments :[e01, e02, e03, e04, e05, e06, e07, e08, e09, e10, e11],borrower: user05, endorser: user_admin, approved : true)
+	
 }
